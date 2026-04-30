ddca6dc0ac074a8f703468db14440e1e5e1fd847
aliyarj <jamadaraliya673@gmail.com>
2026-04-30 18:41:06 +0530

    fix port

--- app.py
+++ app.py
@@ -78,4 +78,5 @@ def about():
     return render_template('about.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8000))
+    app.run(host='0.0.0.0', port=port, debug=False)
